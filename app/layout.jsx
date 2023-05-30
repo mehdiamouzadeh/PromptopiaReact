cbfd28f6d6eea7f62c25fe6825e4475dc10aa1ea
mehdi amouzadeh <mehdi amouzadeh@DESKTOP-T5IUEP6>
2023-05-30 14:52:55 +0330

    add new files

---
+++ app/layout.jsx
@@ -0,0 +1,15 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title:""
+}
+
+
+const layout = () => {
+    console.log();
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
