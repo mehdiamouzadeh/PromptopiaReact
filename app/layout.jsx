ab3a62c93e3cbfd2271205cd0b6d7c88b74a48c1
mehdi amouzadeh <mehdi amouzadeh@DESKTOP-T5IUEP6>
2023-05-31 15:58:05 +0330

    fix issues

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,26 @@
 import '@styles/globals.css'
 
 export const metadata = {
-    title:""
+    title:"Promptopia",
+    description:"Discover and Share AI Prompts"
 }
 
 
-const layout = () => {
+const RootLayout = ({children}) => {
     console.log();
   return (
-    <div>layout</div>
+    <html lang='eng'>
+      <body>
+         <div className='main'>
+            <div className='gradient'/>
+         </div>
+
+         <main className='app'>
+          {children}
+         </main>
+      </body>
+    </html>
   )
 }
 
-export default layout
+export default RootLayout
